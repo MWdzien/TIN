3e0e1c07ede25b42391cbec2f48d31b9ecf6b15b
MWdzien <106245588+MWdzien@users.noreply.github.com>
2024-12-17 01:54:58 +0100

    Update database.ts

--- react2/backend/src/database/database.ts
+++ react2/backend/src/database/database.ts
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise";
 const pool = mysql.createPool({
     host: 'localhost',
     user: "root",
-    password: "Kocilla77!",
+    password: "",
     database: 'flower_shop',
 });
 
